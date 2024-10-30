0ef7c8d93d34a6f9f7b8affd71e98bc464cc9f69
krasi <krasimir.xristov@gmail.com>
2024-10-30 20:25:35 +0100

    add the new routes

---
+++ app/game/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>wishes</div>;
+};
+
+export default page;
---
+++ app/memories/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>memories</div>;
+};
+
+export default page;
