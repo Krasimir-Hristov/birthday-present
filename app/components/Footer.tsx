17597aedb7e0c4c9b7458211037aaa4daf009ae5
krasi <krasimir.xristov@gmail.com>
2024-10-30 21:05:39 +0100

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='flex items-center font-extrabold text-white text-5xl  justify-center w-full bg-purple-600 py-4 shadow-md sticky top-0 z-50'>
+      <h1>ОБИЧАМЕ ТЕ!!!</h1>
+    </div>
+  );
+};
+
+export default Footer;
